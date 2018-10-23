db20f220b47c997b3710ab353623647524208d0f
cocoahoo <huminwei@vip.qq.com>
2018-10-23 15:25:03 +0800

    feat: 完成Android组建

---
+++ android/src/main/java/cn/bookln/rn/imagecropper/CropActivity.java
@@ -0,0 +1,234 @@
+package cn.bookln.rn.imagecropper;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageOptions;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
+import java.io.File;
+import java.io.IOException;
+
+
+public class CropActivity extends AppCompatActivity implements CropImageView.OnSetImageUriCompleteListener,
+        CropImageView.OnCropImageCompleteListener {
+
+    private CropImageView mCropImageView;
+
+    private ImageView IvStartCropper;
+
+    private ImageView IvCloseCropper;
+
+    private Uri mCropImageUri;
+
+    private CropImageOptions mOptions;
+
+    @Override
+    @SuppressLint("NewApi")
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop_image);
+        mCropImageView = findViewById(R.id.cropImageView);
+        IvStartCropper = findViewById(R.id.btn_start_cropper);
+        IvCloseCropper = findViewById(R.id.btn_close_cropper);
+
+        IvCloseCropper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResultCancel();
+            }
+        });
+
+        IvStartCropper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cropImage();
+            }
+        });
+
+
+        Bundle bundle = getIntent().getBundleExtra(CropImage.CROP_IMAGE_EXTRA_BUNDLE);
+        mCropImageUri = bundle.getParcelable(CropImage.CROP_IMAGE_EXTRA_SOURCE);
+        mOptions = bundle.getParcelable(CropImage.CROP_IMAGE_EXTRA_OPTIONS);
+
+        if (savedInstanceState == null) {
+            if (CropImage.isReadExternalStoragePermissionsRequired(this, mCropImageUri)) {
+                // request permissions and handle the result in onRequestPermissionsResult()
+                requestPermissions(
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        CropImage.PICK_IMAGE_PERMISSIONS_REQUEST_CODE);
+            } else {
+                // no permissions required or already grunted, can start crop image activity
+                mCropImageView.setImageUriAsync(mCropImageUri);
+            }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mCropImageView.setOnSetImageUriCompleteListener(this);
+        mCropImageView.setOnCropImageCompleteListener(this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mCropImageView.setOnSetImageUriCompleteListener(null);
+        mCropImageView.setOnCropImageCompleteListener(null);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        setResultCancel();
+    }
+
+    @Override
+    @SuppressLint("NewApi")
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // handle result of pick image chooser
+        if (requestCode == CropImage.PICK_IMAGE_CHOOSER_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_CANCELED) {
+                // User cancelled the picker. We don't have anything to crop
+                setResultCancel();
+            }
+
+            if (resultCode == Activity.RESULT_OK) {
+                mCropImageUri = CropImage.getPickImageResultUri(this, data);
+
+                // For API >= 23 we need to check specifically that we have permissions to read external
+                // storage.
+                if (CropImage.isReadExternalStoragePermissionsRequired(this, mCropImageUri)) {
+                    // request permissions and handle the result in onRequestPermissionsResult()
+                    requestPermissions(
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            CropImage.PICK_IMAGE_PERMISSIONS_REQUEST_CODE);
+                } else {
+                    // no permissions required or already grunted, can start crop image activity
+                    mCropImageView.setImageUriAsync(mCropImageUri);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        if (requestCode == CropImage.PICK_IMAGE_PERMISSIONS_REQUEST_CODE) {
+            if (mCropImageUri != null
+                    && grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // required permissions granted, start crop image activity
+                mCropImageView.setImageUriAsync(mCropImageUri);
+            } else {
+                Toast.makeText(this, "Cancelling, required permissions are not granted", Toast.LENGTH_LONG).show();
+                setResultCancel();
+            }
+        }
+    }
+
+    @Override
+    public void onCropImageComplete(CropImageView view, CropImageView.CropResult result) {
+        setResult(result.getUri(), result.getError(), result.getSampleSize());
+    }
+
+    @Override
+    public void onSetImageUriComplete(CropImageView view, Uri uri, Exception error) {
+        if (error == null) {
+            if (mOptions.initialCropWindowRectangle != null) {
+                mCropImageView.setCropRect(mOptions.initialCropWindowRectangle);
+            }
+            if (mOptions.initialRotation > -1) {
+                mCropImageView.setRotatedDegrees(mOptions.initialRotation);
+            }
+        } else {
+            setResult(null, error, 1);
+        }
+    }
+
+    protected void cropImage() {
+        if (mOptions.noOutputImage) {
+            setResult(null, null, 1);
+        } else {
+            Uri outputUri = getOutputUri();
+            mCropImageView.saveCroppedImageAsync(
+                    outputUri,
+                    mOptions.outputCompressFormat,
+                    mOptions.outputCompressQuality,
+                    mOptions.outputRequestWidth,
+                    mOptions.outputRequestHeight,
+                    mOptions.outputRequestSizeOptions);
+        }
+    }
+
+    protected void rotateImage(int degrees) {
+        mCropImageView.rotateImage(degrees);
+    }
+
+    protected Uri getOutputUri() {
+        Uri outputUri = mOptions.outputUri;
+        if (outputUri == null || outputUri.equals(Uri.EMPTY)) {
+            try {
+                String ext =
+                        mOptions.outputCompressFormat == Bitmap.CompressFormat.JPEG
+                                ? ".jpg"
+                                : mOptions.outputCompressFormat == Bitmap.CompressFormat.PNG ? ".png" : ".webp";
+                outputUri = Uri.fromFile(File.createTempFile("cropped", ext, getCacheDir()));
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to create temp file for output image", e);
+            }
+        }
+        return outputUri;
+    }
+
+    /**
+     * Result with cropped image data or error if failed.
+     */
+    protected void setResult(Uri uri, Exception error, int sampleSize) {
+        int resultCode = error == null ? RESULT_OK : CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE;
+        setResult(resultCode, getResultIntent(uri, error, sampleSize));
+        finish();
+    }
+
+    /**
+     * Cancel of cropping activity.
+     */
+    protected void setResultCancel() {
+        setResult(RESULT_CANCELED);
+        finish();
+    }
+
+    /**
+     * Get intent instance to be used for the result of this activity.
+     */
+    protected Intent getResultIntent(Uri uri, Exception error, int sampleSize) {
+        CropImage.ActivityResult result =
+                new CropImage.ActivityResult(
+                        mCropImageView.getImageUri(),
+                        uri,
+                        error,
+                        mCropImageView.getCropPoints(),
+                        mCropImageView.getCropRect(),
+                        mCropImageView.getRotatedDegrees(),
+                        mCropImageView.getWholeImageRect(),
+                        sampleSize);
+        Intent intent = new Intent();
+        intent.putExtras(getIntent());
+        intent.putExtra(CropImage.CROP_IMAGE_EXTRA_RESULT, result);
+        return intent;
+    }
+}
--- android/src/main/java/cn/bookln/rn/imagecropper/RNYuntiImageCropperModule.java
+++ android/src/main/java/cn/bookln/rn/imagecropper/RNYuntiImageCropperModule.java
@@ -1,17 +1,42 @@
 package cn.bookln.rn.imagecropper;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+
+import com.facebook.react.bridge.ActivityEventListener;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.bridge.WritableNativeMap;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageOptions;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
-public class RNYuntiImageCropperModule extends ReactContextBaseJavaModule {
+import static android.app.Activity.RESULT_CANCELED;
+import static android.app.Activity.RESULT_OK;
+import static com.theartofdev.edmodo.cropper.CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE;
+import static com.theartofdev.edmodo.cropper.CropImage.CROP_IMAGE_EXTRA_OPTIONS;
+import static com.theartofdev.edmodo.cropper.CropImage.CROP_IMAGE_EXTRA_SOURCE;
+
+public class RNYuntiImageCropperModule extends ReactContextBaseJavaModule implements ActivityEventListener {
 
     private final ReactApplicationContext reactContext;
+    private Uri mSource;
+    private CropImageOptions mOptions;
+    private Promise mPromise;
 
     public RNYuntiImageCropperModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+        reactContext.addActivityEventListener(this);
+        mOptions = new CropImageOptions();
     }
 
     @Override
@@ -20,7 +45,72 @@ public class RNYuntiImageCropperModule extends ReactContextBaseJavaModule {
     }
 
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement
+    public void cropWithUri(final String uriStr, final Promise promise) {
+        cropWithUri(uriStr, null, promise);
+    }
+
+    /**
+     * 以Uri打开截图
+     *
+     * @param uriStr 必须是本地Uri
+     * @param options
+     * @param promise
+     */
+    public void cropWithUri(final String uriStr, final ReadableMap options, final Promise promise) {
+        final Activity activity = getCurrentActivity();
+
+        if (activity == null) {
+            promise.reject("400", "Activity doesn't exist");
+            return;
+        }
+
+        mPromise = promise;
+//        setConfiguration(options);
+
+//        mSource = Uri.parse("file:///storage/emulated/0/DCIM/Camera/IMG_20181012_154422.jpg");
+        mSource = Uri.parse(uriStr);
+        activity.startActivityForResult(getIntent(activity), CROP_IMAGE_ACTIVITY_REQUEST_CODE);
+    }
+
+    public Intent getIntent(@NonNull Context context) {
+        mOptions.validate();
+
+        Intent intent = new Intent();
+        intent.setClass(context, CropActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(CROP_IMAGE_EXTRA_SOURCE, mSource);
+        bundle.putParcelable(CROP_IMAGE_EXTRA_OPTIONS, mOptions);
+        intent.putExtra(CropImage.CROP_IMAGE_EXTRA_BUNDLE, bundle);
+        return intent;
+    }
+
+    //TODO 以base64打开截图
+    @ReactMethod
+    public void cropWithBase64(final String base64, final ReadableMap options, final Promise promise) {
+
+    }
+
+
+    @Override
+    public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
+        if (requestCode == CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                WritableMap writableMap = new WritableNativeMap();
+                writableMap.putString("uri", resultUri.toString());
+                mPromise.resolve(writableMap);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+                mPromise.reject("500", "文件路径为空加载图片失败");
+            } else if (resultCode == RESULT_CANCELED) {
+                mPromise.reject("501", "取消操作");
+            }
+        }
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+
     }
 }
